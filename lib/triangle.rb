f9c4875cc00ce5e01d09876a62691b714a044ec4
Wendy Harris <wendyv510@hotmail.com>
2019-11-12 21:31:59 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,22 @@
 class Triangle
-  # write code here
-end
+  
+  attr_accessor :equilateral, :isosceles, :scalene 
+  
+  def initialize(length_1,length_2,lenght_3)
+  end 
+  
+  def kind(length_1,length_2,length_3)
+    if length_ == length_2 == lenght_3 
+      puts "equilateral"
+    elsif (length_1 == length_2)not.eq_to length_3 
+      puts "isosceles" 
+    else (length_1, length_2, length_3)not.eq 
+      puts "scalene"
+    end 
+    
+
+class TriangleError < StandardError 
+end 
+
+
+end 
